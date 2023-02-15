e8a72f7af42d41bd59bd6ce526d1c69c92340200
apjavier2 <apjavier27@gmail.com>
2023-02-15 09:03:24 +0800

    added UI design for add new task scaffold

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -14,6 +14,7 @@ class HomeController extends GetxController {
   final deleting = false.obs;
   //tasks list for home page and make it observable.
   final tasks = <Task>[].obs;
+  final task = Rx<Task?>(null);
 
   @override
   void onInit() {
@@ -37,6 +38,10 @@ class HomeController extends GetxController {
 
   void changeDeleting(bool value) {
     deleting.value = value;
+  }
+
+  void changeTask(Task? select) {
+    task.value = select;
   }
 
   bool addTask(Task task) {
--- lib/app/modules/home/widgets/add_dialog.dart
+++ lib/app/modules/home/widgets/add_dialog.dart
@@ -11,22 +11,93 @@ class AddDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  icon: const Icon(Icons.close)),
-              TextButton(
-                  onPressed: () {},
-                  child: Text('Done', style: TextStyle(fontSize: 14.0.sp)))
-            ],
-          )
-        ],
+      body: Form(
+        key: homeCtrl.formKey,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(3.0.wp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        homeCtrl.editctrl.clear();
+                        homeCtrl.changeTask(null);
+                        Get.back();
+                      },
+                      icon: const Icon(Icons.close)),
+                  TextButton(
+                      //this removes the splash effect / converts it to transparent
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(Colors.transparent)),
+                      onPressed: () {},
+                      child: Text('Done', style: TextStyle(fontSize: 14.0.sp)))
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: Text('New Task',
+                  style: TextStyle(
+                      fontSize: 20.0.sp, fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: TextFormField(
+                controller: homeCtrl.editctrl,
+                decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!))),
+                autofocus: true,
+                validator: ((value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Please enter your todo item';
+                  }
+                  return null;
+                }),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: 5.0.wp, left: 5.0.wp, right: 5.0.wp, bottom: 2.0.wp),
+              child: Text(
+                'Add to',
+                style: TextStyle(fontSize: 14.0.sp, color: Colors.grey),
+              ),
+            ),
+            ...homeCtrl.tasks
+                .map((element) => Obx(
+                      () => InkWell(
+                        onTap: () => homeCtrl.changeTask(element),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 3.0.wp, horizontal: 5.0.wp),
+                          child: Row(
+                            children: [
+                              Icon(
+                                IconData(element.icon,
+                                    fontFamily: 'MaterialIcons'),
+                                color: HexColor.fromHex(element.color),
+                              ),
+                              SizedBox(width: 3.0.wp),
+                              Text(element.title,
+                                  style: TextStyle(
+                                      fontSize: 12.0.sp,
+                                      fontWeight: FontWeight.bold)),
+                              const Spacer(),
+                              if (homeCtrl.task.value == element) ...[
+                                const Icon(Icons.check, color: Colors.blue)
+                              ]
+                            ],
+                          ),
+                        ),
+                      ),
+                    ))
+                .toList()
+          ],
+        ),
       ),
     );
   }
